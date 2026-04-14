19f35d5b4548c418b8995a34bd57de7e7c5fee02
unknown <valenciano3192006@gmail.com>
2026-04-14 09:55:28 +0200

    connect to the databse

--- lib/src/main/java/relationaldbs/dao/UserDaoImpl.java
+++ lib/src/main/java/relationaldbs/dao/UserDaoImpl.java
@@ -1,5 +1,9 @@
 package relationaldbs.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import relationaldbs.model.User;
@@ -10,10 +14,29 @@ import relationaldbs.model.User;
  * 10 abr 2026
  */
 public class UserDaoImpl implements UserDao{
+	
+	private final static String dbURL = "jdbc:postgresql://localhost:5432/postgres";
+	private final static String username = "postgres";
+	private final static String password = "admin";
 
 	@Override
 	public boolean insert(User User) {
+		//the ingredients that we need to do the task
+		//insert sql
 		String insertSQL = "insert into users (name, password, balance, dni, email, age, tlf, adress)" + "values (?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		try (
+			//create an object of Connection to establish a network connection with the database used in our program
+			Connection conn = DriverManager.getConnection(dbURL, username, password);
+			//create an object of PreparedStatement which allows us to prepare, send and execute sqls
+			PreparedStatement ps = conn.prepareStatement(insertSQL)) {
+			ps.setString(1, User.getName());
+			ps.setString(2, User.getPassword());
+			//ps.setString();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 		return false;
 	}
 
